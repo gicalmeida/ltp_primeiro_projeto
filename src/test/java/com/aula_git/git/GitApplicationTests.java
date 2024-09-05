13a2b603fc46be085dd9ba54d33301070b0d7b2d
unknown <gicalmeidaa@gmail.com>
2024-09-05 16:17:11 -0300

    Criação do projeto

---
+++ src/test/java/com/aula_git/git/GitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aula_git.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
